febdbd516f94c9f86cb8a699a28c89b8dece6286
Steven Herbst <sherbst@stanford.edu>
2020-09-23 17:13:33 -0700

    basic command-line options are in place

--- regress.sh
+++ regress.sh
@@ -1,11 +1,20 @@
+# go into the build directory
 mkdir build
 cd build
 
+# generate the Makefile
 cmake -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_BUILD_TYPE=Release  ..
 
+# build the code
 make -j8
+
+# run tests
 ./svinst-slang ../tests/pass/class.sv
+./svinst-slang --define MODULE_NAME=module_name_from_define --define EXTRA_INSTANCE ../tests/pass/def_test.sv
+./svinst-slang --include ../tests/pass ../tests/pass/inc_test.sv
 ./svinst-slang ../tests/pass/intf.sv
 ./svinst-slang ../tests/pass/pkg.sv
 ./svinst-slang ../tests/pass/simple.sv
 ./svinst-slang ../tests/pass/test.sv
+./svinst-slang ../tests/pass/multi/define1.v ../tests/pass/multi/test1.sv ../tests/pass/multi/define2.v \
+    ../tests/pass/multi/dut.v
--- src/svinst-slang.cpp
+++ src/svinst-slang.cpp
@@ -1,3 +1,8 @@
+// references:
+// https://github.com/MikePopoloski/slang/blob/61dcad52297ae0350c90d13fd390c5d618caa47d/tools/driver/driver.cpp
+// https://github.com/Kuree/kratos/blob/a1e695e402fa680f7b06cff0bf4d572bd3670f7c/src/generator.cc#L25-L70
+// https://github.com/Kuree/kratos/blob/a1e695e402fa680f7b06cff0bf4d572bd3670f7c/src/util.cc#L100-L107
+
 #include <iostream>
 
 #include "cxxopts.hpp"
@@ -7,23 +12,67 @@
 #include "slang/text/SourceManager.h"
 #include "slang/diagnostics/DiagnosticEngine.h"
 #include "slang/diagnostics/TextDiagnosticClient.h"
+#include "slang/parsing/Preprocessor.h"
 #include "slang/util/OS.h"
 
-bool is_valid_verilog(const std::string &src) {
-    slang::SourceManager source_manager;
-    slang::Compilation compilation;
-    auto tree = slang::SyntaxTree::fromFile(src, source_manager);
-    compilation.addSyntaxTree(tree);
-    auto &diagnostics = compilation.getParseDiagnostics();
+bool is_valid_verilog(const std::vector<std::string> & files,
+                      const std::vector<std::string> & defines,
+                      const std::vector<std::string> & includes,
+                      bool separate) {
+    // define user-specified macros
+    slang::PreprocessorOptions ppoptions;
+    ppoptions.predefines = defines;
+    ppoptions.predefineSource = "<command-line>";
 
+    // specify include directories
+    // TODO: should addSystemDirectory be used instead?
+    slang::SourceManager sourceManager;
+    for (const std::string& dir : includes) {
+        try {
+            sourceManager.addUserDirectory(string_view(dir));
+        }
+        catch (const std::exception&) {
+            slang::OS::print("error: include directory '{}' does not exist\n", dir);
+        }
+    }
+
+    // read in all of the files
+    std::vector<slang::SourceBuffer> buffers;
+    for (const std::string& file : files) {
+        slang::SourceBuffer buffer = sourceManager.readSource(file);
+        if (!buffer) {
+            slang::OS::print("error: no such file or directory: '{}'\n", file);
+            continue;
+        }
+        buffers.push_back(buffer);
+    }
+
+    // collect all of the options
+    slang::Bag options;
+    options.set(ppoptions);
+
+    // parse the files
+    slang::Compilation compilation(options);
+    if (!separate) {
+        compilation.addSyntaxTree(slang::SyntaxTree::fromBuffers(buffers, sourceManager, options));
+    }
+    else {
+        for (const slang::SourceBuffer& buffer : buffers)
+            compilation.addSyntaxTree(slang::SyntaxTree::fromBuffer(buffer, sourceManager, options));
+    }
+
+    // set up diagnostics printing
     slang::DiagnosticEngine diagEngine(*compilation.getSourceManager());
     auto client = std::make_shared<slang::TextDiagnosticClient>();
     client->setColorsEnabled(true);
     diagEngine.addClient(client);
 
-    for (auto& diag : compilation.getParseDiagnostics())
+    // walk through all of the diagnostic messages
+    auto& diagnostics = compilation.getParseDiagnostics();
+    for (auto& diag : diagnostics)
         diagEngine.issue(diag);
 
+    // print out diagnostics
     std::string diagStr = client->getString();
     slang::OS::print("{}", diagStr);
 
@@ -35,8 +84,8 @@ int main(int argc, char** argv) {
 
     options
         .add_options()
-        ("d,define", "Define", cxxopts::value<std::vector<std::string>>())
-        ("i,include", "Include path", cxxopts::value<std::vector<std::string>>())
+        ("d,define", "Define", cxxopts::value<std::vector<std::string>>()->default_value(""))
+        ("i,include", "Include path", cxxopts::value<std::vector<std::string>>()->default_value(""))
         ("ignore-include", "Ignore any includes", cxxopts::value<bool>()->default_value("false"))
         ("full-tree", "Show the full syntax tree rather than just module instantiation",
          cxxopts::value<bool>()->default_value("false"))
@@ -55,8 +104,11 @@ int main(int argc, char** argv) {
     }
 
     auto files = result["files"].as<std::vector<std::string>>();
+    auto defines = result["define"].as<std::vector<std::string>>();
+    auto includes = result["include"].as<std::vector<std::string>>();
+    auto separate = result["separate"].as<bool>();
 
-    if (is_valid_verilog(files[0])) {
+    if (is_valid_verilog(files, defines, includes, separate)) {
         std::cout << "Valid :-)" << std::endl;
     } else {
         std::cout << "Invalid :-(" << std::endl;
